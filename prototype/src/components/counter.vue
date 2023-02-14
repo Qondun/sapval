c438985685ae4089a039e75fba71a7aea10b32f2
Qondun <robin.gurbuz.0439@student.uu.se>
2023-02-14 16:08:08 +0100

    init commit

---
+++ prototype/src/components/counter.vue
@@ -0,0 +1,27 @@
+<script>
+import { ref } from 'vue';
+import PrimaryButton from './PrimaryButton.vue';
+import DangerButton from './DangerButton.vue';
+
+const count = ref(0);
+
+export default {
+       name: "Counter",
+       props: {
+       	      count: Int
+       }
+}
+</script>
+
+<template>
+  <div class="w-full mt-8 flex justify-center">
+    <span ref="counter" class="text-3xl font-bold">{{ count }}</span>
+  </div>
+  <div class="w-full mt-4 flex flex-row space-x-4 justify-center">
+    <primary-button ref="decrementButton" title="-" @click="count--" />
+    <primary-button ref="incrementButton" title="+" @click="count++" />
+  </div>
+  <div class="w-full mt-4 flex flex-row space-x-4 justify-center">
+    <danger-button ref="resetButton" title="Reset" @click="count = 0" />
+  </div>
+</template>
