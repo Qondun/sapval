f91ec6c11d51fa960753dfa1b616700ae2da87ee
Jennifer Gross <36174212+soothingjennyg@users.noreply.github.com>
2023-03-14 20:28:12 +0100

    working on it.  now finding .json

--- sapval-prototype/src/components/WardOverviewChart.vue
+++ sapval-prototype/src/components/WardOverviewChart.vue
@@ -1,38 +1,61 @@
+
+<template>
+  <!--     <h1>Ward Warnings</h1>
+       <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" /> 
+      <Bar
+      id="ward_warning_chart"
+      :options="chartOptions"
+      :data="chartData"
+    /> -->
+  </template>
+  
+
 <script>
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import wardsdata from '../records/wards.json';
+
+ 
+import Papa from 'papaparse';
+import axios from 'axios';
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+/* console.log(Papa.parse('../records/wards.csv')) */
 
 export default {
   name: 'WardOverviewChart',
-  components: { Bar },
-  data() {
+/*   components: { Bar },
+ */  data: function() {
     return {
-      chartData: {
-      
-        labels: [ 'January', 'February', 'March' ],
+      stuff: wardsdata.wardsInfo,
+    };
+  },
+
+/*   
+       chartData: {
+        labels: [ ],
         datasets: [{
-          label: 'THIS DATA',
-          data: [40, 20, 12] } ]
+          label: '',
+          data: [] } ] 
       },
       chartOptions: {
         responsive: true
       }
     }
-  }
-}
+  }, */
+  mounted: function() {
+    // Checking if everything works, delete this right after you see that everything works
+    console.log(this.stuff);
+  },
+/*     created() {
+      wardsdata.accounts.map((item) => {
+        this.chartData.labels.push( item.name );
+        this.chartData.datasets.push( {label: item.name, "data": [item.pet]} );
+      });
+    } */
+};
 </script>
 
-<template>
-    <h1>Ward Warnings</h1>
-    <!-- <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />  -->
-    <Bar
-    id="ward_warning_chart"
-    :options="chartOptions"
-    :data="chartData"
-  />
-</template>
 
 <style>
 /* chart {
