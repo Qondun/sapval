3db984276656cd3bc4252f443e104848396df396
Jennifer Gross <36174212+soothingjennyg@users.noreply.github.com>
2023-03-22 13:46:26 +0100

    updated to correct data array, still need data updates

--- sapval-prototype/src/components/WardOverviewChart.vue
+++ sapval-prototype/src/components/WardOverviewChart.vue
@@ -2,7 +2,7 @@
 
 <script>
 import { Bar } from 'vue-chartjs'
-import {defineStore} from "pinia"
+import { defineStore } from "pinia"
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, BarController } from 'chart.js'
 import wardsdata from '../records/inpatientWards.json';
 import patientWarnings from '../records/PatientWarnings.json';
@@ -14,15 +14,16 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 
 export default {
-    name: "WardOverviewChart",
-       components: { Bar },
-      data: function () {
-        return {
+  name: "WardOverviewChart",
+  components: { Bar },
+  data: function () {
+    return {
       chartData: {
-        labels: [ ],
-        datasets: [ 
-          { label: '1',
-            data: [], 
+        labels: [],
+        datasets: [
+          {
+            label: '1',
+            data: [],
             backgroundColor: '#fff2cc',
           }, {
             label: '2',
@@ -42,10 +43,10 @@ export default {
             label: '5',
             data: [],
             backgroundColor: '#190263'
-          } ]
+          }]
       },
       chartOptions: {
-        
+
         responsive: true,
         scales: {
           x: {
@@ -55,67 +56,77 @@ export default {
             stacked: true
           }
         }
-        //stacked: true
       },
-      
+
       ward: wardsdata.wardsInfo,
       patient: patientWarnings.WarningInfo,
       warnings: warningList.WarningInfo
 
     }
-    
-    },
-      created: function severityLevelReturn(warningNumberToFind) {
-        warning = warnings.find(warnings.warningNumber(warningNumberToFind))
-        return warning.getElementById("severityLevel")
 
-    },
+  },
+  created: function severityLevelReturn(warningNumberToFind) {
+    warning = warnings.find(warnings.warningNumber(warningNumberToFind))
+    return warning.getElementById("severityLevel")
 
-      created: function () {
-        // Checking if everything works, delete this right after you see that everything works
-       // console.log(this.ward);  
+  },
 
-        let wardNameList = []
-        let wardWarningsArr = [0,0,0,0,0]
-        var value = 0
-        console.log(wardWarningsArr)
+  created: function () {
+    // Checking if everything works, delete this right after you see that everything works
+    // console.log(this.ward);  
 
-
-
-        console.log("printmenow!")      
-        this.ward.map((item) => {
-         // console.log(item.Avdelning + " : " + item.numPlatser);
-          this.chartData.labels.push(item.Address)
-          wardNameList.push(item.Address)
-          console.log(wardNameList)
-          this.chartData.datasets[0].data.push(parseInt(item.numPlatser))
-        })
-        let len = wardNameList.length;
-        console.log(len);
-        //let wardWarningArrayList = [len][wardWarningsArr]
-       // var wardWarningArray = [...Array(len)].map(e => Array(5).fill(1));
-        var wardWarningArray = [...Array(5)].map(e => Array(len).fill(1));
+    let wardNameList = []
+    let wardWarningsArr = [0, 0, 0, 0, 0]
+    var value = 0
+    console.log(wardWarningsArr)
 
 
-       // this.chartData.datasets[1].data.push(parseInt(wardWarningArray[0]))
-        this.chartData.datasets[1].data = wardWarningArray[0].map(x => x);
-        console.log(wardWarningArray)
-        console.log(this.chartData.datasets[1])
+
+    console.log("printmenow!")
+    this.ward.map((item) => {
+      // console.log(item.Avdelning + " : " + item.numPlatser);
+      this.chartData.labels.push(item.Address)
+      wardNameList.push(item.Address)
+      console.log(wardNameList)
+      //this.chartData.datasets[0].data.push(parseInt(item.numPlatser))
+    })
+    let len = wardNameList.length;
+    console.log(len);
+    //let wardWarningArrayList = [len][wardWarningsArr]
+    // var wardWarningArray = [...Array(len)].map(e => Array(5).fill(1));
+    var wardWarningArray = [...Array(5)].map(e => Array(len).fill(1));
 
 
-    },
-    //components: { BarController }
-    persist: true
+    // this.chartData.datasets[1].data.push(parseInt(wardWarningArray[0]))
+    this.chartData.datasets[0].data = wardWarningArray[0].map(x => x);
+    console.log(wardWarningArray)
+    console.log(this.chartData.datasets[0])
+
+    this.chartData.datasets[1].data = wardWarningArray[1].map(x => x);
+    console.log(wardWarningArray)
+    console.log(this.chartData.datasets[1])
+
+    this.chartData.datasets[2].data = wardWarningArray[2].map(x => x);
+    console.log(wardWarningArray)
+    console.log(this.chartData.datasets[2])
+
+    this.chartData.datasets[3].data = wardWarningArray[3].map(x => x);
+    console.log(wardWarningArray)
+    console.log(this.chartData.datasets[3])
+
+    this.chartData.datasets[4].data = wardWarningArray[4].map(x => x);
+    console.log(wardWarningArray)
+    console.log(this.chartData.datasets[4])
+
+
+  },
+  //components: { BarController }
+  persist: true
 };
 </script>
 
-<template>      
-  <Bar
-      id="ward-warning-chart"
-      :options="chartOptions"
-      :data="chartData"
-    />
-    
+<template>
+  <Bar id="ward-warning-chart" :options="chartOptions" :data="chartData" />
 </template>
 
 <style>
