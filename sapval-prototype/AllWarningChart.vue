f296be008fbf195b0a13b0734a4aa7549ed9cb27
Jennifer Gross <36174212+soothingjennyg@users.noreply.github.com>
2023-03-29 13:40:46 +0200

    test

---
+++ sapval-prototype/AllWarningChart.vue
@@ -0,0 +1,93 @@
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { defineStore } from "pinia"
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, BarController } from 'chart.js'
+import wardsdata from '../records/inpatientWards.json';
+import patientWarnings from '../records/PatientWarnings.json';
+import warningList from '../records/warningList.json';
+import patientInformation from '../records/patientInformation.json';
+import { storeToRefs } from 'pinia'
+import { watch } from 'vue'
+import { useOverallWarningsStore } from '../stores/warningsOverall'
+import { useWarningsByWardStore } from '../stores/warningsByWard'
+
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+    name: "AllWarningChart",
+    components: { Bar },
+    watch: {
+        chartData: {
+            handler(newData, oldData) {
+                // Maybe force chart to refresh here...?
+                // Note: if you resize the window the chart does refresh itself.
+                console.log("Data updated..")
+            },
+            deep: true,
+        },
+    },
+    data: function () {
+        const warningsStore = useOverallWarningsStore()
+        warningsStore.initialize()
+        const warningsStoreRef = storeToRefs(useOverallWarningsStore())
+        console.log("allocated warningsStore for allWarningChart")
+        return {
+            storeData: warningsStore,
+            chartData: warningsStoreRef.getChartData, // This should keep it responsive since its a reference?
+            chartOptions: {
+                responsive: true,
+                scales: {
+                    x: {
+                        stacked: true,
+                    },
+                    y: {
+                        stacked: true
+                    }
+                }
+            },
+
+            ward: wardsdata.wardsInfo,
+            patient: patientWarnings.WarningInfo,
+            warnings: warningList.WarningInfo,
+            patientInfo: patientInformation.PatientAlertDrug
+
+        }
+
+    },
+    computed: {
+        // a computed getter
+        warningsChartData() {
+            // `this` points to the component instance
+            return this.storeData.getChartData
+        },
+        chartOptionsComputed() {
+            return this.chartOptions
+        }
+    },
+    created: function () {
+
+
+    },
+    //components: { BarController }
+    persist: true
+};
+</script>
+
+<template>
+    <Bar ref="theChart" id="all-warning-chart" :options="chartOptionsComputed" :data="chartData" />
+    <button @click="this.storeData.decreaseWarningNumberArray(3)" type="button">TestAllWarningChart</button>
+</template>
+
+<style>
+/* chart {
+    background-color: #c41a1a;
+    background: #FFFFFF;
+    border: none;
+    border-radius: 2px;
+    box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);
+    width: "640";
+    height: "480";
+} */
+</style>
